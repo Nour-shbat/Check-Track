88f921d2bfbca845b4e56f92fac9f043ef5bbfa2
Verhalle,Koen K.G.T. <460975@student.fontys.nl>
2025-05-30 12:16:44 +0200

    new

--- Check&Track/Form1.cs
+++ Check&Track/Form1.cs
@@ -19,7 +19,7 @@ namespace Check_Track
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello world!");
+            MessageBox.Show("Hello world!!");
         }
     }
 }
