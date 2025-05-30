398835c5d08908b84156bb070e5a26b6098dd288
Nour Shbat <nour1991,sy@gmail.com>
2025-05-30 12:47:07 +0200

    tttt

---
+++ Check&Track/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Track
+{
+    class Passenger
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string SeatNumber { get; set; }
+        public Passenger(string name, int age, string seatNumber)
+        {
+            Name = name;
+            Age = age;
+            SeatNumber = seatNumber;
+        }
+        public override string ToString()
+        {
+            return $"{Name}, Age: {Age}, Seat: {SeatNumber}";
+        }
+    }
+}
